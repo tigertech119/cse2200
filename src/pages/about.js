78b78564cc91a2cd88df213d8e204e5bfb1ea55c
hasan100_official <hasan100.official@gmail.com>
2025-07-29 22:08:00 +0600

    Updated the about page with a paragraph about myself

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,7 +15,19 @@ const About = () => {
       <Button variant="secondary" className="m-2">
         Stay on About
       </Button>
+
+         <h6>About Myself</h6>
+         <p>I am a student of Computer Science and Engineering who wants to explore 
+            and learn alot in his life . I live in Dhaka , Bangladesh . I am currently 
+            studying in Ahansullah Univarsity of Science and Technology . My aim in life 
+            is to build amazing software that helps our people and inspires others to do 
+            good in life . Hope to see you again . We can connect through my email - 
+            hasan.official@gmail.com , LinkedIn - hasan02 
+         </p>
+
     </div>
+
+     
   );
 };
 
